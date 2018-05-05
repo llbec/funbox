5ebf40448b9c9e8164d7aa9e0444b8ce7e38b92b
bicliu <klins@163.com>
2018-05-05 09:41:23 +0800

    add cpp

---
+++ tools/sudoku/lib/sudoku_x.cpp
@@ -0,0 +1,60 @@
+#include "sudoku_x.h"
+
+template <uint XVAL>
+Sudoku_X<XVAL>::Sudoku_X()
+    {
+        for(uint i = 0; i < WIDTH; i++)
+            for(uint j = 0; j < WIDTH; j++)
+            {
+                form[i][j].SetNull();
+                grid[(i/X_2)*X_2+(j/X_2)][(i%X_2)*X_2+(j%X_2)] = &form[i][j];
+            }
+    }
+
+template <uint XVAL>
+void Sudoku_X<XVAL>::SetNull()
+{
+    for(uint i = 0; i < WIDTH; i++)
+        for(uint j = 0; j < WIDTH; j++)
+            form[i][j].SetNull();
+}
+
+template <uint XVAL>
+bool Sudoku_X<XVAL>::IsFinish(){ return (0 == iresult);}
+
+template <uint XVAL>
+bool Sudoku_X<XVAL>::CheckFinish()
+{
+    for(uint i = 0; i < WIDTH; i++)
+        for(uint j = 0; j < WIDTH; j++)
+        {
+            if(0 == form[i][j].fix)
+                return false;
+        }
+    
+    return true;
+}
+
+template <uint XVAL>
+void Sudoku_X<XVAL>::Show()
+{
+    std::cout << X_2 << " X " << X_2 << " sudoku:" << std::endl;
+    for(uint i = 0; i < WIDTH; i++)
+    {
+        for(uint j = 0; j < WIDTH; j++)
+        {
+            printf("%3d ", form[i][j].fix);
+            if((j+1)%X_2 == 0)
+                printf("  ");
+        }
+        printf("\n");
+        if((i+1)%X_2 == 0)
+            printf("\n");
+    }
+}
+
+template <uint XVAL>
+void Sudoku_X<XVAL>::SetForm(uint x, uint y, uint8 value)
+{
+    
+}
--- tools/sudoku/lib/sudoku_x.h
+++ tools/sudoku/lib/sudoku_x.h
@@ -51,58 +51,7 @@ protected:
     unit_t * grid[WIDTH][WIDTH];
     uint iresult = WIDTH * WIDTH;
 public:
-    Sudoku_X()
-    {
-        for(uint i = 0; i < WIDTH; i++)
-            for(uint j = 0; j < WIDTH; j++)
-            {
-                form[i][j].SetNull();
-                grid[(i/X_2)*X_2+(j/X_2)][(i%X_2)*X_2+(j%X_2)] = &form[i][j];
-            }
-    }
-
-    void SetNull()
-    {
-        for(uint i = 0; i < WIDTH; i++)
-            for(uint j = 0; j < WIDTH; j++)
-                form[i][j].SetNull();
-    }
-
-    bool IsFinish(){ return (0 == iresult);}
-
-    bool CheckFinish()
-    {
-        for(uint i = 0; i < WIDTH; i++)
-            for(uint j = 0; j < WIDTH; j++)
-            {
-                if(0 == form[i][j].fix)
-                    return false;
-            }
-        
-        return true;
-    }
-
-	void Show()
-	{
-		std::cout << X_2 << " X " << X_2 << " sudoku:" << std::endl;
-		for(uint i = 0; i < WIDTH; i++)
-		{
-			for(uint j = 0; j < WIDTH; j++)
-			{
-				printf("%3d ", form[i][j].fix);
-				if((j+1)%X_2 == 0)
-                	printf("  ");
-			}
-			printf("\n");
-			if((i+1)%X_2 == 0)
-				printf("\n");
-		}
-	}
-
-    void UpdateForm(uint x, uint y)
-    {
-        
-    }
+    
 };
 
 #endif
