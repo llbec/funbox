dd1f4a3c313e9ea906a50ec6d87b8ece074ceb1a
pazzabec <klins@163.com>
2018-09-18 18:53:07 +0800

    update

--- tools/cmd/libs/readchar.h
+++ tools/cmd/libs/readchar.h
@@ -1,9 +1,47 @@
 #ifndef READCHAR_H
 #define READCHAR_H
 #include <stdio.h>
-#include <conio.h>
 #include <string>
 #include <vector>
+
+/*
+ * __GNUC        => linux
+ * __MSC_VER    => window
+ */
+#ifdef __GNUC__  // depend on GCC
+
+#include <termio.h>
+/*
+ * get one char from  input
+ * return: char
+ */
+int sh_getch(void) {
+    int cr;
+    struct termios nts, ots;
+
+    if (tcgetattr(0, &ots) < 0) // 得到当前终端(0表示标准输入)的设置
+        return EOF;
+
+    nts = ots;
+    cfmakeraw(&nts); // 设置终端为Raw原始模式，该模式下所有的输入数据以字节为单位被处理
+    if (tcsetattr(0, TCSANOW, &nts) < 0) // 设置上更改之后的设置
+        return EOF;
+
+    cr = getchar();
+    if (tcsetattr(0, TCSANOW, &ots) < 0) // 设置还原成老的模式
+        return EOF;
+
+    return cr;
+}
+
+#elif _MSC_VER // depend on Visual Studio
+
+#include <conio.h>
+#define sh_getch    _getch
+
+#else
+    #error "error : Currently only supports the Visual Studio and GCC!"
+#endif
 
 template<class Type>
 class CReadChar
@@ -51,7 +89,7 @@ public:
     void HandlerBacksoace() {
         vecLine_.pop_back();
         bTab_ = false;
-        printf("\b \b\0");
+        printf("\b \b%c",'\0');
     }
 
     void HandlerEnter() {
@@ -82,7 +120,7 @@ public:
 
     void Run(){
         while(true){
-            cGet_ = getch();
+            cGet_ = sh_getch();
             Handler();
         }
     }
