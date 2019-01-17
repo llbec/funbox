5b32e737facbec1c8ac0291e0e99b6e89fb20e7e
pazzabec <klins@163.com>
2019-01-17 13:56:57 +0800

    update

--- script-projects/python/186Host.py
+++ script-projects/python/186Host.py
@@ -20,10 +20,11 @@ class Host :
     
 g_Parser = argparse.ArgumentParser("SSH login host 10.186.11.x")
 #g_Parser.add_argument('IP subnet', metavar='Subnet', type=str, nargs='+', help='the subnet value of the 10.186.11. ')
-g_Parser.add_argument('subnet', metavar='Subnet', type=int, help='the subnet value of the 10.186.11. ')
+g_Parser.add_argument('subnet', metavar='Subnet', type=int, help='the subnet value of the 10.186.11. ', nargs='+')
 g_Parser.add_argument('-u', '--usr', type=str, default='root', metavar='', help='ssh usr name, default is root')
 g_Parser.add_argument('-p', '--port', type=int, default=22, metavar='', help='ssh port, default is 22')
 g_Parser.add_argument('-s', '--passwd', type=str, default='Zxcvbn2018', metavar='', help='usr\'s password, default is Zxcvbn2018')
 
-falgs = g_Parser.parse_args()
-print('%d,%s,%d,%s'%(falgs.subnet, falgs.usr, falgs.port, falgs.passwd))
+#falgs = g_Parser.parse_args()
+#print('%d,%s,%d,%s'%(falgs.subnet, falgs.usr, falgs.port, falgs.passwd))
+print(g_Parser.parse_args())
