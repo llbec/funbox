0b002caeea5c327436e272fb065045f6b1fa2cb8
pazzabec <klins@163.com>
2019-04-10 14:37:55 +0800

    update

--- script-projects/python/login.py
+++ script-projects/python/login.py
@@ -11,14 +11,9 @@ class Host :
         self.alias = _alias
         self.relay = _relay
 
-relayHosts = [
-    Host('47.75.73.38', 22, 'ubuntu', '', 'hk.pem', 'HK-relay', -1),
-    Host('114.67.40.11', 20282, 'lbc', 'lbc', '', '186-relay', -1),
-]
-
 hosts = [
     Host('45.77.223.88', 22, 'root', '9Y[zVY3PPS+GXBN]', '', '666MN', -1),
-    Host('114.67.40.11', 20282, 'lbc', 'lbc', '', '186-relay', -1),
+    Host('114.67.40.11', 20282, 'lbc', 'lbc', '', '186', -1),
     Host('10.186.11.27', 22, 'root', 'Zxcvbn2018', '', '186-27', 1),
 ]
 
@@ -29,7 +24,7 @@ def ssh_passwd(_ssh, _host) :
         if _i == 1 :
             return _i
         else :
-            _host.passwd = input('Enter the password:')
+            _host.passwd = input('Enter the password for %s:'%(_host.ip))
             continue
     _n = -1
     return _n
@@ -106,21 +101,3 @@ while True :
         continue
     
     myssh(_idx)
-
-    '''if hosts[_idx].relay < 0 :
-        _ssh = pexpect.spawn(getLoginCmd(hosts[_idx]))
-    else :
-        _ssh = pexpect.spawn(getLoginCmd(relayHosts[hosts[_idx].relay]))
-        if ssh_login(_ssh, relayHosts[hosts[_idx].relay]) < 0 :
-            print('[ERROR]Connect relay host fails, try chmod 700 *.pem')
-            continue
-        _ssh.sendline(getLoginCmd(hosts[_idx]))
-    if ssh_login(_ssh, hosts[_idx]) < 0 :
-        print('[ERROR]Connect fails')
-        _ssh.close()
-        continue
-
-    _rows, _columns = os.popen('stty size', 'r').read().split()
-    _ssh.setwinsize(int(_rows), int(_columns))
-    print('Welcome to %s\n'%(hosts[_idx].ip))
-    _ssh.interact()'''
