998c4958b3ac1cfb6077fb2c62f309f35eaec141
pazzabec <klins@163.com>
2019-10-31 15:52:44 +0800

    update

--- script-projects/python/delivertx.py
+++ script-projects/python/delivertx.py
@@ -2,21 +2,6 @@
 
 import os, sys, argparse, json, getpass
 
-listSecret = []
-listRcv = []
-redeemScript = "532102e781a1e50d1b2868f1416a90889eebbd427d50cfec16f2c660244f4d704b1b9621030941e5c318708929ea61975996e2b1dbe4b4b6776d69bc2b7f6fc9e16be3b239210356f5a9acdf355094d34912d25eb5f2b02b1b095656a94892ee6f6ddbc94e1d2253ae"
-script = "a91488ad225351c77286c5c3fc11695ba8a5065d41b687"
-
-class validateAddress :
-    def __init__(self, _listSecret, _threshold, _addr, _redeem, _script) :
-        self.Secrets = _listSecret
-        self.Threshold = _threshold
-        self.addr = _addr
-        self.Redeem = _redeem
-        self.script = _script
-
-srcAddr = validateAddress(listSecret, 3, "SZkgLVprTwMNBPzvSgCfrGtdFADyThzMPA", redeemScript, script)
-
 def rpcwd(_m, *_params):
     _ps = ''
     for _p in _params:
